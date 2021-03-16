9f0da1b63b080cb75fc9a04598c67ac494000212
Sarah Norris <sarah@sekai.co.uk>
2021-03-16 18:13:59 +0000

    chore: initial calculator setup

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,53 +1,178 @@
 import './Calculator.css';
 
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			'& > *': {
-				margin: theme.spacing(1),
-				width: '25ch',
-			},
-		},
-	})
-);
+import React from 'react';
 
 function Calculator() {
-	const classes = useStyles();
+	const [
+		numOfVisitorsControl,
+		setNumOfVisitorsControl,
+	] = React.useState<number>(0);
+	const [
+		numOfConversionsControl,
+		setNumOfConversionsControl,
+	] = React.useState<number>(0);
+	const [
+		conversionRateControl,
+		setConversionRateControl,
+	] = React.useState<number>(0);
+	const [
+		standardErrorControl,
+		setStandardErrorControl,
+	] = React.useState<number>(0);
+
+	const [
+		numOfVisitorsVariant,
+		setNumOfVisitorsVariant,
+	] = React.useState<number>(0);
+	const [
+		numOfConversionsVariant,
+		setNumOfConversionsVariant,
+	] = React.useState<number>(0);
+	const [
+		conversionRateVariant,
+		setConversionRateVariant,
+	] = React.useState<number>(0);
+	const [
+		standardErrorVariant,
+		setStandardErrorVariant,
+	] = React.useState<number>(0);
+
+	const [calculatedSig, setCalculatedSig] = React.useState<number>(0);
+	const [pValue, setPValue] = React.useState<number>(0);
+
+	const handleControlVisitorChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNumOfVisitorsControl(parseInt(e.target.value));
+	};
+
+	const handleControlConversionChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNumOfConversionsControl(parseInt(e.target.value));
+	};
+
+	const handleVariantVisitorChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNumOfVisitorsVariant(parseInt(e.target.value));
+	};
+
+	const handleVariantConversionChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNumOfConversionsVariant(parseInt(e.target.value));
+	};
+
+	const calculateSignificance = () => {
+		setConversionRateControl(
+			numOfConversionsControl / numOfVisitorsControl
+		);
+		setStandardErrorControl(
+			Math.sqrt(
+				(conversionRateControl * (1 - conversionRateControl)) /
+					numOfVisitorsControl
+			)
+		);
+
+		setConversionRateVariant(
+			numOfConversionsVariant / numOfVisitorsVariant
+		);
+		setStandardErrorVariant(
+			Math.sqrt(
+				(conversionRateVariant * (1 - conversionRateVariant)) /
+					numOfVisitorsVariant
+			)
+		);
+
+		setCalculatedSig(1);
+	};
 
 	return (
 		<div className="calculator">
-			<Container maxWidth="lg">
+			<div>
 				<header className="calculator-header">
 					<h1>Statistical Significance Calculator</h1>
 				</header>
-				<form className={classes.root} noValidate autoComplete="off">
-					<TextField
-						id="visitors"
-						label="Number of Visitors"
-						type="number"
-						InputLabelProps={{
-							shrink: true,
-						}}
-					/>
-					<TextField
-						id="conversions"
-						label="Number of Conversions"
-						type="number"
-						InputLabelProps={{
-							shrink: true,
-						}}
-					/>
-					<Button variant="contained" color="primary" type="submit">
-						Calculate
-					</Button>
-				</form>
-			</Container>
+				<div>
+					<form noValidate autoComplete="off">
+						<table>
+							<thead>
+								<tr>
+									<th></th>
+									<th>Visitors</th>
+									<th>Conversions</th>
+									<th>Conversion Rate</th>
+									<th>Standard Error</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Control</td>
+									<td>
+										<input
+											id="visitors"
+											type="number"
+											value={numOfVisitorsControl}
+											onChange={
+												handleControlVisitorChange
+											}
+										/>
+									</td>
+									<td>
+										<input
+											id="conversions"
+											type="number"
+											value={numOfConversionsControl}
+											onChange={
+												handleControlConversionChange
+											}
+										/>
+									</td>
+									<td>{conversionRateControl}</td>
+									<td>{standardErrorControl}</td>
+								</tr>
+								<tr>
+									<tr>Variant</tr>
+									<td>
+										<input
+											id="visitors"
+											type="number"
+											value={numOfVisitorsVariant}
+											onChange={
+												handleVariantVisitorChange
+											}
+										/>
+									</td>
+									<td>
+										<input
+											id="conversions"
+											type="number"
+											value={numOfConversionsVariant}
+											onChange={
+												handleVariantConversionChange
+											}
+										/>
+									</td>
+									<td>{conversionRateVariant}</td>
+									<td>{standardErrorVariant}</td>
+								</tr>
+							</tbody>
+						</table>
+
+						<button onClick={() => calculateSignificance()}>
+							Calculate
+						</button>
+					</form>
+				</div>
+				<div>
+					<h3>Significance</h3>
+					<strong>Z-score:</strong> {calculatedSig}
+					<br />
+					<strong>P-value:</strong> {pValue}
+					<br />
+				</div>
+			</div>
 		</div>
 	);
 }
