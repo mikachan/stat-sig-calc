1dfcb80b934f76d4f46c56458d8f65c7006e8282
Sarah Norris <sarah@sekai.co.uk>
2021-03-16 16:34:01 +0000

    Revert "feat: install tailwindcss; add initial form elements"
    This reverts commit 44c8ff216f35ae15e48b96885fd5f468e17adac9.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
+import './App.css';
+
 function App() {
 	return (
-		<div className="bg-green-600 text-center">
-			<div className="flex flex-col items-center justify-center text-white h-screen">
-				<header>
-					<h1 className="text-2xl">
-						Statistical Significance Calculator
-					</h1>
-				</header>
-				<div className="text-white">
-					<div>
-						<label>
-							<p>Visitors:</p>
-							<input type="number" name="visitors" />
-						</label>
-					</div>
-					<div>
-						<label>
-							<p>Conversions:</p>
-							<input type="number" name="conversions" />
-						</label>
-					</div>
-				</div>
-			</div>
+		<div className="App">
+			<header className="App-header">
+				<h1>Statistical Significance Calculator</h1>
+			</header>
 		</div>
 	);
 }
